c80c62bfdb3ce2223a4602ebda5a6e46362d052a
bpun1p <barry_pun@hotmail.com>
2020-11-18 17:40:49 -0800

    home's Header component have been test for content to render correctly

--- client/src/components/home/Header.test.js
+++ client/src/components/home/Header.test.js
@@ -1,1 +1,12 @@
+import * as React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom'
 
+import Header from './Header';
+
+test('renders the correct content', () => {
+  const { getByTestId } = render(<Header />);
+
+  expect(getByTestId('header-logo')).not.toBeNull();
+  expect(getByTestId('navBtn')).not.toBeNull();
+});
