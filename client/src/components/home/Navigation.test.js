14bb1096bb22868f28de34a664cfb3750a557f32
bpun1p <barry_pun@hotmail.com>
2020-11-18 17:43:33 -0800

    tested for content to render correctly

--- client/src/components/home/Navigation.test.js
+++ client/src/components/home/Navigation.test.js
@@ -1,3 +1,18 @@
+import * as React from 'react';
+import { render, fireEvent, queryByTestId } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import '@testing-library/jest-dom';
 
+import Navigation from './Navigation';
 
-
+test('renders the correct content', () => {
+  const mockFunction = jest.fn();
+  const { getByTestId, getByText } = render (
+    <Navigation popUpHandler={mockFunction}/>, { wrapper: BrowserRouter }
+  );
+    
+  expect(getByText('LOGIN')).not.toBeNull();
+  expect(getByText('SIGN UP')).not.toBeNull();
+  expect(getByText('ABOUT ME')).not.toBeNull();    
+  expect(getByTestId('exitNavBtn')).not.toBeNull();
+});
