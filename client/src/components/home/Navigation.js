e40fee57e59527a8e6f81d599e8bd7a28f7dfa80
bpun1p <barry_pun@hotmail.com>
2020-11-18 17:39:53 -0800

    home's Center component have been tested for content to render correctly

--- client/src/components/home/Navigation.js
+++ client/src/components/home/Navigation.js
@@ -7,7 +7,7 @@ function Navigation(props) {
 
   return (
     <nav className="header__nav">
-      <Link to="/login">
+      <Link data-testid="login-btn" to="/login">
         <button type="button" className="header__login">LOGIN</button>
       </Link>
       <Link to="/register">
@@ -16,7 +16,7 @@ function Navigation(props) {
       <Link to="/about-me">
         <button type="button" className="header__aboutme">ABOUT ME</button>
       </Link>
-      <button type="button" className="close__nav" onClick={popUpHandler}>
+      <button type="button" data-testid="exitNavBtn" className="close__nav" onClick={popUpHandler}>
         <svg alt="exit icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path className="header__exit-icon" d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z" fill="#13182C" />
         </svg>
