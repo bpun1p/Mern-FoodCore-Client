2f2748808d8476fd50087f7e45eed89e8f8416d3
bpun1p <barry_pun@hotmail.com>
2020-12-11 23:35:48 -0800

    back up branch for Main

---
+++ client/src/components/Utils/LoadingIndicator.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { usePromiseTracker } from 'react-promise-tracker';
+import Loader from 'react-loader-spinner';
+
+export default function LoadingIndicator() {
+  const { promiseInProgress } = usePromiseTracker();
+  return (
+    promiseInProgress
+        && (
+        <div
+          style={{
+            width: '100',
+            height: '70vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Loader type="ThreeDots" color="#e67e22" height="100" width="100" />
+        </div>
+        )
+  );
+}
