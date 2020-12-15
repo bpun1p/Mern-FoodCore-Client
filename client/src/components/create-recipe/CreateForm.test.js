f63eea9e4ede6ae8363eeba091e035c82d3a8706
bpun1p <barry_pun@hotmail.com>
2020-12-14 19:06:25 -0800

    removed unused query

--- client/src/components/create-recipe/CreateForm.test.js
+++ client/src/components/create-recipe/CreateForm.test.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { render, fireEvent, act, getAllByText } from '@testing-library/react';
+import { render, fireEvent, act } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import AuthService from '../../service/AuthService';
 import RecipeService from '../../service/RecipeService';
