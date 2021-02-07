1937d52b45e27e82792feb72ffefda226fc9790b
bpun1p <barry_pun@hotmail.com>
2021-02-07 11:05:37 -0800

    redux action.types compiled

---
+++ client/src/actions/types.js
@@ -0,0 +1,10 @@
+export const FETCH_USER_POSTS = 'FETCH_USER_POSTS';
+export const POST_TO_USER = 'NEW_POST';
+
+export const FETCH_ALL_POSTS = 'FETCH_ALL_POSTS';
+export const POST_TO_ALL = 'NEW_POST';
+
+export const USER_SIGNUP = 'USER_SIGNUP';
+export const USER_LOGIN = 'USER_LOGIN';
+export const USER_LOGOUT = 'USER_LOGOUT';
+export const USER_AUTHENTICATE = 'USER_AUTHENTICATE';
