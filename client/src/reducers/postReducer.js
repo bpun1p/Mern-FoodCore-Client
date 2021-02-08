ef201f7a611fa8c74e92216ae1d5feca481ed314
bpun1p <barry_pun@hotmail.com>
2021-02-07 21:32:07 -0800

    postReducer refractored to eslint standards

--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -1,38 +1,38 @@
-/* eslint-disable */
-import { 
-    FETCH_USER_POSTS, 
-    POST_TO_USER,
-    FETCH_ALL_POSTS,
-    POST_TO_ALL } from '../actions/types';
+import {
+  FETCH_USER_POSTS,
+  POST_TO_USER,
+  FETCH_ALL_POSTS,
+  POST_TO_ALL,
+} from '../actions/types';
 
 const initialState = {
   fetchUsersPosts: [],
   postToUser: {},
-  fetchAllPosts : [],
-  postToAll: {}
+  fetchAllPosts: [],
+  postToAll: {},
 };
 
-export default function(state = initialState, action) {
+export default function (state = initialState, action) {
   switch (action.type) {
     case FETCH_USER_POSTS:
       return {
         ...state,
-        fetchUsersPosts: action.payload
+        fetchUsersPosts: action.payload,
       };
     case POST_TO_USER:
       return {
         ...state,
-        postToUser: action.payload
+        postToUser: action.payload,
       };
     case FETCH_ALL_POSTS:
       return {
         ...state,
-        fetchAllPosts: action.payload
+        fetchAllPosts: action.payload,
       };
     case POST_TO_ALL:
       return {
         ...state,
-        postToAll: action.payload
+        postToAll: action.payload,
       };
     default:
       return state;
