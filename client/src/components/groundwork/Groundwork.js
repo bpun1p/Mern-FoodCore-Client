1f5ec4abf0dc060435c7c1e5d9f21b0a0660e0a5
bpun1p <barry_pun@hotmail.com>
2021-02-10 19:12:47 -0800

    onclick handlers added for toggling out of recipes when clicking the background

--- client/src/components/groundwork/Groundwork.js
+++ client/src/components/groundwork/Groundwork.js
@@ -142,7 +142,7 @@ export default function Groundwork() {
         </ul>
       </div>
       {isPopUp ?
-        <div className="groundworks__code-container">
+        <div className="groundworks__code-container" onClick={togglePopUp}>
             <svg className="modal__exit" data-testid="exit-modal-btn" onClick={togglePopUp} width="50" height="50" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path className="modal__exit-icon" d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z" fill="#13182C" />
             </svg>
